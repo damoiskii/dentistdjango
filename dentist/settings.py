669ee8d7807a337fa491ae75d0fa11d7fe77d2c3
Damoi Myers <moimyazz@gmail.com>
2020-07-24 23:05:26 -0500

    Settings Updated

--- dentist/settings.py
+++ dentist/settings.py
@@ -131,7 +131,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Another way to send email is to do as follows...
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
